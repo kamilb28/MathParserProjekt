09d808c3a379fb24a767294d55f225d2af20fb03
Kamil <72549797+kamilb28@users.noreply.github.com>
2022-03-31 13:16:25 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,18 +1,22 @@
 import re
 
+# analiza leksykalna/ czytanie tokenów
 def lex_analyisis(string):
     symbols = ['+', '-', '*', '/']
     brackets = ['(', ')']
 
     tokens = list()
     for index, char in enumerate(string):
+        # if it's symbol 
         if char in symbols:
             tokens.append([char, "symbol"])
+            
+        # or bracket
         elif char in brackets:
             tokens.append([char, "bracket"])
 
         # https://www.guru99.com/python-regular-expressions-complete-tutorial.html
-        # if its o number
+        # if it's number
         elif re.match(r'\d', char):
             tokens.append([char, "number"])
 
@@ -20,7 +24,7 @@ def lex_analyisis(string):
         elif char == ' ':
             pass
 
-        # isnt number or symbol
+        # isn't number or symbol
         else:
             raise Exception('Invalid token')
     return tokens
